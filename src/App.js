6790cdeabad5dedf5a89c13c9f44c84838da55e3
David Amador <54488581+dsamador@users.noreply.github.com>
2023-03-03 06:55:43 -0500

    Tailwind config and Provider

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import Error404 from "containers/errors/Error404";
+import Home from "containers/pages/Home"
+
+import { Provider } from "react-redux";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div>
+    <Provider>
       
-    </div>
+    </Provider>
   );
 }
 
