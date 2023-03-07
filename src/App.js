ef07ea12a35bfa779c276f0b842c1e007a29b8a3
David Amador <54488581+dsamador@users.noreply.github.com>
2023-03-07 09:04:33 -0500

    Store created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import Error404 from "containers/errors/Error404";
 import Home from "containers/pages/Home"
-
+import store from "store";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 
 function App() {
   return (
-    <Provider >
+    <Provider store={store}>
       <Router>
         {/* Error Display */}
         <Route path="*" element={<Error404/>}/>
